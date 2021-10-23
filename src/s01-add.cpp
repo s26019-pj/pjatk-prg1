d177fe2960289c4b27c874f5dc6a82181912e3e8
Dorian Opacki <s26019@pjwstk.edu.pl>
2021-10-23 18:29:49 +0200

    small fix

--- src/s01-add.cpp
+++ src/s01-add.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 #include <string>
 
-auto main(int argc, char* argv[]) -> int
-{
-	if(argc < 2){
-	std::cout << "I need 2 numbers to add\n";
-	return 0;
-	}
-	
+auto main(int, char* argv[]) -> int
+{	
 	auto const firstArgument = std::stoi(argv[1]);
 	auto const secondArgument = std::stoi(argv[2]);
 	auto const result = firstArgument + secondArgument;
--- src/s01-sub.cpp
+++ src/s01-sub.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 #include <string>
 
-auto main(int argc, char* argv[]) -> int
+auto main(int, char* argv[]) -> int
 {
-	if(argc < 2){
-	std::cout << "I need 2 numbers to subtract\n";
-	return 0;
-	}
-	
 	auto const firstArgument = std::stoi(argv[1]);
 	auto const secondArgument = std::stoi(argv[2]);
 	auto const result = firstArgument - secondArgument;
