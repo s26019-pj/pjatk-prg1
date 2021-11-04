8f8ef485f52600bdf8b0537c4461a3b4738b9a83
Dorian Opacki <dorian.opacki@sollers.eu>
2021-11-04 20:51:41 +0100

    Extend logic with user input

---
+++ src/s01-mul-stdin.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+auto get_user_input(std::string prompt) -> int
+{
+    std::cout << prompt << "Number:";
+
+    auto number = std::string{};
+    std::getline(std::cin, n);
+
+    return std::stoi(number);
+}
+
+auto main(int, char* argv[]) -> int
+{	
+    auto const promptQuestion = "Please provide your number";
+	auto const firstArgument = get_user_input(promptQuestion);
+	auto const secondArgument = get_user_input(promptQuestion);
+	auto const result = firstArgument * secondArgument;
+	std::cout << result << "\n";
+    
+	return 0;
+}
+
